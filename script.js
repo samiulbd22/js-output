e8d45cad6243699e624e7512b09b35ef1c9a8804
samiulbd22 <samiul.bitm22@gmail.com>
2022-10-12 16:38:34 +0600

    upade

---
+++ script.js
@@ -0,0 +1,14 @@
+var message1 = "welcome to window alert";
+var message2 = "welcome to window";
+var message3 = "welcome to document.write";
+var message4 = "welcome to innerHTML";
+var message5 = "welcome to console";
+// window.alert(message1);
+//alert(message1);
+document.write(message3);
+document.getElementById("in").innerHTML=message4;
+var variable =document.getElementById("in");
+console.log(message5);
+console.log(variable);
+
+
